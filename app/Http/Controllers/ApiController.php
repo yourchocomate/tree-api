673758cec6a15b0a0b89a1cf592dbf721533d4c9
yourchocomate <yourchocomate@gmail.com>
2023-08-12 18:42:53 +0600

    Added skills to api

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -10,9 +10,10 @@ class ApiController extends Controller
 {
     public function index(Profile $profile)
     {
-        $socials = $profile->user->socials()->where('status', 1)->get();
-        $portfolios = $profile->user->portfolios()->where('status', 1)->get();
-        
+        $skills = $profile->user->skills()->where('status', 1)->get(['id', 'label', 'icon', 'tooltip', 'description', 'url', 'created_at']);
+        $socials = $profile->user->socials()->where('status', 1)->get(['id', 'name', 'url', 'icon', 'created_at']);
+        $portfolios = $profile->user->portfolios()->where('status', 1)->get(['id', 'label', 'icon', 'tooltip', 'description', 'url', 'created_at']);
+
         return response()->json([
             'user' => [
                 'name' => $profile->user->name,
@@ -22,6 +23,7 @@ class ApiController extends Controller
                 'avater' => Storage::disk('public')->url($profile->avater),
             ],
             'handles' => [
+                'skill' => $skills,
                 'social' => $socials,
                 'portfolio' => $portfolios,
             ]
